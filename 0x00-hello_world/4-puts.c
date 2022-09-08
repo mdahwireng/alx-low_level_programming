10695702d36a86dbdb509a910d0a523ae9dc77b6
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-08 16:46:06 -0700

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 
-/** function 
-* returning an int */
 int main(void)
-{
-	/* function returning an int */
-	puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
