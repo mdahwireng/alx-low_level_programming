6899af6813f57a71cfd195151204dc3a9e7d9222
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-13 16:14:14 -0700

    T2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+# include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@
 
 int printword(char *sentence);
 int print_alphabet(void);
+void print_alphabet_x10(void);
