0fb8420f52a0502fd7da1d901c42ee6100c61ba5
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-13 22:24:57 -0700

    T11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,17 +16,18 @@ void print_to_98(int n)
 		{
 			printf(", %d", n);
 		}
-		printf(", 98");
+		printf(", 98 ");
 	}
 	else
 	{
 		if (n < 98)
 	{
+		n++
 		for (; n < 98; n++)
 		{
 			printf(", %d", n);
 		}
-		printf(", 98");
+		printf(", 98 ");
 	}
 	}
 }
