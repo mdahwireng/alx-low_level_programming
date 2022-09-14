b7e94fc9bac6916ca6f98e8b4c31f545efe38b3b
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-13 19:58:50 -0700

    T5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+# include "main.h"
+
+/**
+ * print_sign - prints the sign of a given integer
+ * @n: The integer to be checked
+ * Retrun: 1 if n > 0 , 0 if n is 0 or -1 if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	putchar('+');
+	return (1);
+	}
+	else
+	{
+	if (n == 0);
+	{
+	putchar('0');
+	return (0);
+	}
+	else
+	{
+	putchar('-');
+	return (-1);
+	}
+	}
+}
