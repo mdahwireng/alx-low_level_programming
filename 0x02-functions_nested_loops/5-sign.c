ae5433027f4779d3d8a68e276eb9362940060ff7
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-13 20:02:06 -0700

    T5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,15 @@ int print_sign(int n)
 	}
 	else
 	{
-	if (n == 0);
-	{
-	putchar('0');
-	return (0);
-	}
-	else
-	{
-	putchar('-');
-	return (-1);
-	}
+		if (n == 0);
+		{
+		putchar('0');
+		return (0);
+		}
+		else
+		{
+		putchar('-');
+		return (-1);
+		}
 	}
 }
