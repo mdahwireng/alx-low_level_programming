1c00d8708f4ee446176c7e0c376f2a5696e13d86
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-15 21:19:07 -0700

    All

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -5,3 +8,5 @@ int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+
+#endif
