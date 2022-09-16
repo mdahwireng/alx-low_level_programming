b45e146aa649461f9bbeadb6d8d693addbdde745
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-15 21:16:06 -0700

    T5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,31 +6,32 @@
  */
 void more_numbers(void)
 {
-	int h;
+        int h;
 
-	for (h = 0; h < 10; h++)
-	{
-		int i;
-		int j;
-		int k;
-		int n[15];
+        for (h = 0; h < 10; h++)
+        {
+                int i;
+                int j;
+                int k;
+                int n[20];
 
-		for (i = 0; i < 10; i++)
-		{
-			j = i + 48;
-			n[i] = j;
+                for (i = 0; i < 10; i++)
+                {
+                        j = i + 48;
+                        n[i] = j;
 
-			if (i < 5)
-			{
-				n[i + 10] = j;
-			}
-		}
+                        if (i < 5)
+                        {
+                                n[2*i + 10] = 49;
+                                n[2*i + 11] = j;
+                        }
+                }
 
-		for (k = 0; k < 15; k++)
-		{
-			_putchar(k);
-		}
-		_putchar('\n');
-	}
-	_putchar('\n');
+                for (k = 0; k < 20; k++)
+                {
+                	_putchar(n[k]);
+                }
+                _putchar('\n');
+        }
+
 }
