454cb0eecba6f0b945d4a7f1ade197ba5c96eb7b
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-15 20:25:47 -0700

    T5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 9, omitting 2 and 4 followed by a new line
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int h;
+	int i;
+	int k;
+	int n[15];
+
+	for (h = 0; h < 10; h++)
+	{
+		int i;
+		int j;
+		int k;
+		int n[15];
+
+		for (i = 48; i < 58; i++)
+		{
+			j = i + 48
+			if (i < 5)
+			{
+				n[i] = j;
+				n[i + 10] = j;
+			}
+		}
+
+		for (k = 0; k < 15; k++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+}
