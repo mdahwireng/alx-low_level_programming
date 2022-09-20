547c58c7a92afe3fd8b5a846ee7862b14f153876
mdahwireng <kaaymike@hotmail.co.uk>
2022-09-19 22:50:24 -0700

    h

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int count = 0;
 
-	i = 0;
-	while (*(s + i) != '\0')
+	while (count >= 0)
 	{
-		i++;
+		if (s[count] == '\0')
+			break;
+		count++;
 	}
-	for (; i >= 0; i--)
-	{
-		_putchar(*(s + i));
-	}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/5-revstring.c
+++ 0x05-pointers_arrays_strings/5-revstring.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * rev_string - a function that reverses a string
